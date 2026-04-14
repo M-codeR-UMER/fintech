7fc8b08412a32550eef5e0b0c8542fa81f65bec5
Muhammad Alber <albermalik111@gmail.com>
2026-04-14 11:07:00 +0500

    Convert mobile configs to ESM for Windows compatibility

--- finpay-mobile/metro.config.js
+++ finpay-mobile/metro.config.js
@@ -1,6 +1,9 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+import { getDefaultConfig } from "expo/metro-config";
+import { withNativeWind } from "nativewind/metro";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+export default withNativeWind(config, { input: "./global.css" });
